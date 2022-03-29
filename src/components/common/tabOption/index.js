127f3626c0b0463d53802de9bb70304c9c6848cb
Shan7xuz <96675411+Shan7xuz@users.noreply.github.com>
2022-03-29 16:28:11 +0530

    tabOption section debuging

--- src/components/common/tabOption/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './tabOption.css';
-
-const TabOption = ({ activeTab, setActiveTab }) => {
-  return <div>TabOption</div>;
-};
-
-export default TabOption;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import Header from '../../components/common/header';
-import TabOptions from '../../components/common/tabOption';
+import TabOptions from '../../components/common/tabOptions';
 import Footer from '../../components/common/footer';
 import Delivery from '../../components/delivery';
 import DiningOut from '../../components/diningOut';
-import Nightlife from '../../components/nightlIfe';
+import Nightlife from '../../components/nightlife';
 
 const HomePage = () => {
   const [activeTab, setActiveTab] = useState('Delivery');
@@ -12,7 +12,7 @@ const HomePage = () => {
   return (
     <>
       <Header />
-      <TabOptions />
+      <TabOptions activeTab={activeTab} setActiveTab={setActiveTab} />
       {getCorrectScreen(activeTab)}
       <Footer />
     </>
