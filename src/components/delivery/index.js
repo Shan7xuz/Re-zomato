c4e65debde261d691f51d21c0c3f18026d027c04
Shan7xuz <96675411+Shan7xuz@users.noreply.github.com>
2022-03-30 16:21:53 +0530

    the filter section is created

--- src/components/delivery/index.js
+++ src/components/delivery/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import './delivery.css';
+import Filters from '../common/filters';
+
+const deliveryFilters = [
+  {
+    id: 1,
+    icon: <i className='fi fi-rr-settings-sliders absolute-center'></i>,
+    title: 'Filter',
+  },
+  { id: 2, title: 'Reating: 4.0+' },
+  { id: 3, title: 'Safe and Hygienic' },
+  { id: 4, title: 'Pure Veg' },
+  {
+    id: 5,
+    title: 'Delivery Time',
+    icon: <i className='fi fi-rr-apps-sort absolute-center'></i>,
+  },
+  { id: 6, title: 'Great Offer' },
+];
 
 const Delivery = () => {
-  return <div>Delivery</div>;
+  return (
+    <>
+      <div className='max-width'>
+        <Filters filterlist={deliveryFilters} />
+      </div>
+    </>
+  );
 };
 
 export default Delivery;
