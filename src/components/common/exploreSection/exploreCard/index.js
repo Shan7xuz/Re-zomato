67c73b3ff2f38a762448df04b8e301f1d45221c7
Shan7xuz <96675411+Shan7xuz@users.noreply.github.com>
2022-04-01 10:34:23 +0530

    card section is complete

--- src/components/common/exploreSection/exploreCard/index.js
+++ src/components/common/exploreSection/exploreCard/index.js
@@ -21,7 +21,54 @@ const ExploreCard = ({ restaurant }) => {
       ? offers[0].text
       : null;
 
-  return <div>ExploreCard</div>;
+  return (
+    <div className='explore-card cur-po'>
+      <div className='explore-card-cover'>
+        <img src={coverImg} alt={name} className='explore-card-image' />
+        <div className='delivery-time'>{deliveryTime}</div>
+        {proOff && <div className='pro-off'>{proOff}</div>}
+        {goldOff && <div className='gold-off absolute-center'>{goldOff}</div>}
+        {discount && <div className='discount absolute-center'>{discount}</div>}
+      </div>
+      <div className='res-row'>
+        <div className='res-name'>{name}</div>
+        {rating && (
+          <div className='res-rating absolute-center'>
+            {rating}
+            <i className='fi fi-rr-star absolute-center'></i>
+          </div>
+        )}
+      </div>
+      <div className='res-row'>
+        {cuisines.length && (
+          <div className='res-cuisine'>
+            {cuisines.map((item, i) => {
+              return (
+                <span className='res-cuisine-tag'>
+                  {item}
+                  {i !== cuisines.length - 1 && ','}
+                </span>
+              );
+            })}
+          </div>
+        )}
+        {approxPrice && <div className='res-price'>{approxPrice}</div>}
+      </div>
+      {bottomContainers.length > 0 && (
+        <div className=''>
+          <div className='card-separator'></div>
+          <div className='explore-bottom'>
+            <img
+              src={bottomContainers[0]?.image?.url}
+              alt={bottomContainers[0].text}
+              style={{ height: '18px' }}
+            />
+            <div className='res-bottom-text'>{bottomContainers[0]?.text}</div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default ExploreCard;
